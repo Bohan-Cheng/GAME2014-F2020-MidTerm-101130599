0568d6ee3c51e1c7cdc5464b9f8c42cabcc9ea67
DepressedCookie <vipc.1224@live.ca>
2020-10-20 21:13:44 -0400

    Changed BG to move vertically

--- Assets/_Scripts/BackgroundController.cs
+++ Assets/_Scripts/BackgroundController.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class BackgroundController : MonoBehaviour
 {
-    public float verticalSpeed;
-    public float verticalBoundary;
+    public float horzontalSpeed;
+    public float horzontalBoundary;
 
     // Update is called once per frame
     void Update()
@@ -16,18 +16,18 @@ public class BackgroundController : MonoBehaviour
 
     private void _Reset()
     {
-        transform.position = new Vector3(0.0f, verticalBoundary);
+        transform.position = new Vector3(horzontalBoundary, 0.0f);
     }
 
     private void _Move()
     {
-        transform.position -= new Vector3(0.0f, verticalSpeed) * Time.deltaTime;
+        transform.position -= new Vector3(horzontalSpeed, 0.0f) * Time.deltaTime;
     }
 
     private void _CheckBounds()
     {
         // if the background is lower than the bottom of the screen then reset
-        if (transform.position.y <= -verticalBoundary)
+        if (transform.position.x <= -horzontalBoundary)
         {
             _Reset();
         }
